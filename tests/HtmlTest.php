77c835cad65ffd09141919156cabc795fe9ba0ce
jos3duardo <jos3duardolopes@gmail.com>
2020-04-16 19:27:09 -0400

    first test with phpunit

---
+++ tests/HtmlTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class HtmlTest extends \PHPUnit\Framework\TestCase
+{
+    public function testEsteEUmSimplesExemploDeTeste()
+    {
+        $this->assertTrue(true);
+    }
+}
